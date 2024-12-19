3d33f42a905c400abcac2d97e7c505f9cfb6ce28
pondersome <karim@compuguru.com>
2024-12-18 19:46:47 -0600

    revert roarm_driver to python package type

--- src/roarm_main/roarm_driver/setup.py
+++ src/roarm_main/roarm_driver/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+import os
+from glob import glob
 
 package_name = 'roarm_driver'
 
@@ -7,9 +9,15 @@ setup(
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
+        # This registers the package as a resource.
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
+        
+        # Install the package.xml
         ('share/' + package_name, ['package.xml']),
+        
+        # Install all launch files into the share/<package_name>/launch directory
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
